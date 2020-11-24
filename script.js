4c51fcfc270e5f7f310267e1837b7137e633455b
Julia Semakhina <contremoi@yandex.ru>
2020-11-24 15:34:09 +0300

    second commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const triggers = document.querySelectorAll('a');
-const highlight = document.createElement('span');
-highlight.classList.add('highlight');
-document.body.appendChild(highlight);
+const pressed = [];
+const secretCode = 'julia';
 
-function highlightLink() {
- const linksCoords = this.getBoundingClientRect();
- console.log(linksCoords);
+window.addEventListener('keyup', (e)=> {
+	console.log(e.key);
+	pressed.push(e.key);
+	pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+	if(pressed.join("").includes(secretCode)){
+		console.log("Yeaah!");
+		cornify_add();
+	}
+	console.log(pressed);
+})
 
- const coords = {
-  width: linksCoords.width, 
-  height: linksCoords.height,
-  top: linksCoords.top + window.scrollY,
-  left: linksCoords.left + window.scrollX
- }
-;
- highlight.style.width = `${coords.width}px`;
- highlight.style.height = `${coords.height}px`;
- highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+function strip(bandName) {
+	return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+const sortedBands = bands.sort( (a,b)=> strip(a) > strip(b) ? 1 : -1);
+console.log(sortedBands);
+
+document.querySelector('#bands').innerHTML = sortedBands.map(elem => `<li>${elem}</li>`).join('');
